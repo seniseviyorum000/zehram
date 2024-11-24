3cbf72c2aa6d41502715dcfc88321ec7e9fe0377
seniseviyorum000 <otizmliyusif@gmail.com>
2024-11-24 23:48:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Şiirler ve iltifatlar zaten HTML'de yer alıyor. Ama ek bir etkileşim eklemek isterseniz buraya yazabilirsiniz.
+});
